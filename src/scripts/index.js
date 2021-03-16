bab54c1a0b5f9476f368635c5f2b128c972cf5a6
JSadept <jsadept4@gmail.com>
2021-03-16 02:44:41 +0200

    Create default config and constructor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,135 @@
-// eslint-disable-next-line max-classes-per-fileclass Slider {	constructor(props) {}	_init() {}	_initSliderSelectors() {}	_createSliderDots() {}	_createSliderButtons() {}	_createInfinityArrays() {}	_createSlider() {}	_render() {}	reRender(direction) {}	start() {}	stop() {}	nextSlide() {}	previousSlide() {}	gotoSlide(index) {}	destroy() {}}class SliderVertical extends Slider {	constructor(props) {		super(props);		this.slidesPerView = 1;		this.slider.classList.add('slider_vertical');	}	start() {}	stop() {}	nextSlide() {}	previousSlide() {}	gotoSlide(index) {}	destroy() {}}class SliderHorizontal extends Slider {	constructor(props) {		super(props);	}	start() {}	stop() {}	nextSlide() {}	previousSlide() {}	gotoSlide(index) {}	destroy() {}}function createSlider(config) {	if (config.direction === 'vertical') {		return new SliderVertical(config);	}	return new SliderHorizontal(config);}// testsconst slider1 = createSlider({	centered: true,	infinity: false,	direction: 'horizontal',	arrows: true,	loop: true,	slidesPerView: 4,	slidesPerScroll: 4,	spaceBetween: 20,});console.dir(slider1);
+const defaultSliderConfig = {
+	arrows: true,
+	activeIndex: 1,
+	centered: true,
+	direction: 'horizontal',
+	dots: false,
+	infinity: false,
+	speed: 3000,
+	spaceBetween: 20,
+	slideId: 0,
+	slidesPerView: 4,
+	slidesPerScroll: 4,
+
+	get slider() {
+		return document.querySelector('.a-slider');
+	},
+	get slidesContainer() {
+		return document.querySelector('.a-slider__slides');
+	},
+	get slidesWrapper() {
+		return document.querySelector('.a-slider__slides-wrapper');
+	},
+	get slides() {
+		return document.querySelectorAll('.a-slide');
+	},
+	get nextBtn() {
+		return document.getElementById('a-slider__arrow_next');
+	},
+	get prevBtn() {
+		return document.getElementById('a-slider__arrow_prev');
+	},
+	// firstClone: slides[0].cloneNode(true),
+	// lastClone: slides[slides.length - 1].cloneNode(true),
+};
+
+class Slider {
+	constructor(props) {
+		// Combining default configuration with user configuration data
+		Object.assign(this, props.defaultConfig, props);
+	}
+
+	_init() {}
+
+	_initSliderSelectors() {}
+
+	_createSliderDots() {}
+
+	_createSliderButtons() {}
+
+	_createInfinityArrays() {}
+
+	_createSlider() {}
+
+	_render() {}
+
+	_cloneSlides() {}
+
+	reRender(direction) {}
+
+	start() {}
+
+	stop() {}
+
+	nextSlide() {}
+
+	previousSlide() {}
+
+	gotoSlide(index) {}
+
+	destroy() {}
+}
+
+class SliderVertical extends Slider {
+	constructor(props) {
+		super(props);
+		this.slidesPerView = 1;
+		this.slider.classList.add('slider_vertical');
+	}
+
+	start() {}
+
+	stop() {}
+
+	nextSlide() {}
+
+	previousSlide() {}
+
+	gotoSlide(index) {}
+
+	destroy() {}
+}
+
+class SliderHorizontal extends Slider {
+	constructor(props) {
+		super(props);
+	}
+
+	start() {}
+
+	stop() {}
+
+	nextSlide() {}
+
+	previousSlide() {}
+
+	gotoSlide(index) {}
+
+	destroy() {}
+}
+
+function createSlider(config, defaultConfig) {
+	if (defaultConfig) config.defaultConfig = defaultConfig;
+	else {
+		config.defaultConfig = defaultSliderConfig;
+	}
+
+	if (config.direction === 'vertical') {
+		return new SliderVertical(config);
+	}
+	return new SliderHorizontal(config);
+}
+
+// tests
+const slider1 = createSlider({
+	centered: true,
+	infinity: false,
+	direction: 'horizontal',
+	arrows: true,
+	loop: true,
+	slidesPerView: 4,
+	slidesPerScroll: 4,
+	spaceBetween: 20,
+	speed: 2000,
+});
+
